5b62f21112436736291e2c1a067bee364dcef0e3
Crypto Bureau Master <152272581+CryptoBureau01@users.noreply.github.com>
2025-01-23 12:59:35 +0530

    Update run-executor.bat

--- run-executor.bat
+++ run-executor.bat
@@ -3,14 +3,14 @@ setlocal
 
 where python >nul 2>nul
 if %errorlevel%==0 (
-    echo menjalankan script executor.py
+    echo cryptobureau script executor.py
     python executor.py
     goto end
 )
 
 where python3 >nul 2>nul
 if %errorlevel%==0 (
-    echo menjalankan script executor.py
+    echo cryptobureau script executor.py
     python3 executor.py
     goto end
 )
